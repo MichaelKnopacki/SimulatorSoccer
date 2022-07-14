93a7977814ac7352626c99a8a573d0776e1ddb3d
Michael Carlos Knopacki <michaelknopacki@hotmail.com>
2022-07-14 16:45:11 -0300

    Correção

--- app/src/main/java/com/example/simulator/ui/MainActivity.java
+++ app/src/main/java/com/example/simulator/ui/MainActivity.java
@@ -79,7 +79,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             });
-            throw new RuntimeException("teste Crshlytics");
         });
     }
 
